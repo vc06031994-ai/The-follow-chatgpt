6a3d4866d8659eea23dbec5f6ca6569d1fa94fee
vc06031994-ai <vc06031994@gmail.com>
2026-09-16 12:42:54 +0530

    Bump authentication plugin for password link fix

--- tfp-authentication/tfp-authentication.php
+++ tfp-authentication/tfp-authentication.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: TFP Authentication
-Version: 1.2.3
+Version: 1.2.4
 */
 if(!defined('ABSPATH')) exit;
-define('TFP_AUTH_VERSION','1.2.3');
+define('TFP_AUTH_VERSION','1.2.4');
 define('TFP_AUTH_PATH', plugin_dir_path(__FILE__));
 define('TFP_AUTH_URL', plugin_dir_url(__FILE__));
 
